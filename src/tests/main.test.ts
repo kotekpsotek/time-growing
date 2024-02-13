8ab03aacc8e2b3f9995ea22fb9779887b0aa2ac3
kotekpsotek <siematumichael@protonmail.com>
2024-02-13 01:53:08 +0100

    Unit testing

---
+++ src/tests/main.test.ts
@@ -0,0 +1,42 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { lastUsages } from "../lib/store/statefull";
+
+beforeAll(() => {
+    lastUsages.update(v => {
+        return [
+            {
+                origin: "https://facebook.com",
+                timestamp: 1200000
+            },
+            {
+                origin: "https://instagram.com",
+                timestamp: 1800000
+            },
+            {
+                origin: "https://abc.com",
+                timestamp: 1000000
+            },
+            {
+                origin: "https://cdk.com",
+                timestamp: 1900000
+            },
+        ]
+    })
+});
+
+describe("Storages unit testing", () => {
+    it("lastUsages content", () => {
+        lastUsages.update(v => {
+            console.info(v);
+            return v;
+        })
+    })
+
+    it("lastUsage recent timestamp", () => {
+        const lastU = lastUsages.recentUsageTimestamp();
+        
+        console.log("Last usage was in", lastU);
+
+        expect(lastU).toBeGreaterThan(0);
+    });
+});
